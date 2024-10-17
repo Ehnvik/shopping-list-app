186b41c549e4d16733dec487bf1bdd762a13b558
ehnvik <gustav.ehnvik@gmail.com>
2024-10-17 15:01:00 +0200

    initial commit

---
+++ config/database.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__ . "/../vendor/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+$host = $_ENV["DB_HOST"];
+$dbname = $_ENV["DB_NAME"];
+$user = $_ENV["DB_USER"];
+$password = $_ENV["DB_PASSWORD"];
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failes: " . $e->getMessage());
+}
